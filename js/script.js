8522941ed2ae628fd6e1c24b42d4bddffc2a5d55
Drumsandcoffee <Tomas.Myklebust@stud.noroff.no>
2022-10-23 21:14:08 +0200

    q8
    Q8 added

--- js/script.js
+++ js/script.js
@@ -81,16 +81,13 @@ let html = "";
 function createCats(cats){
 
     for( let i = 0; i < cats.length; i++){
-        
-      
-
-
+        if(cats[i].age == null){
+            cats[i].age = "Age Unknown"
+        }      
     html += `<div>
                 <h5>${cats[i].name}</h5>
                 <p>${cats[i].age}</p>
-             </div>`
-
-  
+             </div>`  
 }}
 createCats(cats)
 
